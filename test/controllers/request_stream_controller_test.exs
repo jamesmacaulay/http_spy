f1270972940cfda0094149f93e39fbdf0b2cdeb8
James MacAulay <jmacaulay@gmail.com>
2015-12-30 00:03:02 -0400

    some tests

--- test/controllers/request_stream_controller_test.exs
+++ test/controllers/request_stream_controller_test.exs
@@ -1,8 +1,30 @@
 defmodule HttpSpy.RequestStreamControllerTest do
   use HttpSpy.ConnCase
+  use HttpSpy.ChannelCase
 
   test "GET /" do
+    conn = get conn(), "/"
+    assert redirected_to(conn) =~ ~r"/[^/]+/spy"
+  end
+
+  test "GET /foo/spy" do
     conn = get conn(), "/foo/spy"
     assert html_response(conn, 200) =~ "<div id=\"elm-main\"></div>"
+  end
+
+  test "GET /foo with some query params" do
+    socket = subscribe_and_join!(socket(), HttpSpy.RequestStreamChannel, "requests:foo")
+    conn = get conn(), "/foo?bar=baz"
+    assert response(conn, 200) == ""
+    assert_broadcast("request", %{method: "GET", path: "/foo", queryString: "bar=baz"})
+  end
+
+  test "POST /foo with some data" do
+    socket = subscribe_and_join!(socket(), HttpSpy.RequestStreamChannel, "requests:foo")
+    conn = conn()
+    |> put_req_header("content-type", "application/json")
+    |> post "/foo", "{\"bar\": \"baz\"}"
+    assert response(conn, 200) == ""
+    assert_broadcast("request", %{method: "POST", path: "/foo", body: "{\"bar\": \"baz\"}"})
   end
 end
